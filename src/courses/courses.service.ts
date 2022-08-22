4a86e0426888b0cb36fd8c2097fd21825747ddaf
Talles Corradini <tallescorradini@gmail.com>
2022-08-22 15:58:33 -0300

    feat: add course module, controllers, services with in-memory db

---
+++ src/courses/courses.service.ts
@@ -0,0 +1,53 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entity';
+
+@Injectable()
+export class CoursesService {
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: 'NestJS fundamentals',
+      description: 'This is the description of nestJS fundamentals',
+      tags: ['node.js', 'nestjs', 'javascript'],
+    },
+  ];
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: string) {
+    const course = this.courses.find((course) => course.id === Number(id));
+
+    if (!course) {
+      throw new HttpException(
+        `Course ID ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    return course;
+  }
+
+  create(createCourseDto: any) {
+    this.courses.push(createCourseDto);
+  }
+
+  update(id: string, updateCourseDto: any) {
+    const courseIndex = this.courses.findIndex(
+      (course) => course.id === Number(id),
+    );
+
+    this.courses[courseIndex] = updateCourseDto;
+  }
+
+  remove(id: string) {
+    const courseIndex = this.courses.findIndex(
+      (course) => course.id === Number(id),
+    );
+
+    if (courseIndex >= 0) {
+      this.courses.splice(courseIndex, 1);
+    }
+  }
+}
